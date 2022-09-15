00f4530297415d43a6c2f11353f4aede0fb8e46b
Julian Krochmal <juliankrochmal@gmail.com>
2022-09-15 20:08:29 -0300

    adding other constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,7 @@
 import { API_KEY } from '../variables'
 
+export const URL = 'https://geo-services-by-mvpc-com.p.rapidapi.com/airports?language=en&countrycode=US'
+
 export const OPTIONS = {
     method: 'GET',
     headers: {
--- src/hooks/useAirports.tsx
+++ src/hooks/useAirports.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react"
-import { OPTIONS } from "../constants"
+import { OPTIONS, URL } from "../constants"
 
 export const useAirports = () => {
-
-    const URL = 'https://geo-services-by-mvpc-com.p.rapidapi.com/airports?language=en&countrycode=US'
     const [airports, setAirports] = useState<any>({})
 
-    const getAllUSAirports = () => {
+    const getUsaAirports = () => {
         fetch(URL, OPTIONS)
             .then(response => response.json())
             .then(response => setAirports(response))
             .catch(err => console.error(err));
     }
 
-    useEffect(() => getAllUSAirports(), [])
+    useEffect(() => getUsaAirports(), [])
 
     return { airports }
 }
